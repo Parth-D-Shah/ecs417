71b0ef2e9f2d4d8853e9d6ce0f5518eab76bd334
ParthShah101 <parthshah2065@outlook.com>
2020-05-01 12:39:33 +0100

    changed

--- webroot/index.php
+++ webroot/index.php
@@ -1,40 +1,91 @@
-<?php
-/**
- * The Front Controller for handling every request
- *
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.2.9
- * @license       MIT License (https://opensource.org/licenses/mit-license.php)
- */
-
-// Check platform requirements
-require dirname(__DIR__) . '/config/requirements.php';
-
-// For built-in server
-if (php_sapi_name() === 'cli-server') {
-    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
-
-    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
-    $file = __DIR__ . $url['path'];
-    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
-        return false;
-    }
-}
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-use App\Application;
-use Cake\Http\Server;
-
-// Bind your application to the server.
-$server = new Server(new Application(dirname(__DIR__) . '/config'));
-
-// Run the request/response through the application and emit the response.
-$server->emit($server->run());
+<!DOCTYPE = html>
+<html>
+<head lang="en">
+  <meta charset="utf-8">
+  <title>Parth Shah</title>
+  <link rel="stylesheet" href="reset.css" type="text/css"/>
+  <link rel="stylesheet" href="homepage.css">
+</head>
+<body>
+  <div class="topnav">
+    <h1>Parth Shah</h1>
+    <a href="blog.php">Blog</a>
+    <a href="Portfolio.html">Portfolio/CV</a>
+    <a href="Experience.php">Experience</a>
+    <a href="Contact.php">Contact</a>
+  </div>
+  <aside>
+    <div class="sidebar">
+      <?php
+        session_start();
+        if(isset($_SESSION['logged_in']))
+        {
+          if($_SESSION['logged_in'] == true)
+          {
+            echo'<a href="logout.php">Log Out</a>';
+          }
+          else if($_SESSION['logged_in'] == false)
+          {
+            echo'<a href="login.html">Log In/Register</a>';
+          }
+        }
+        else
+        {
+          echo'<a href="login.html">Log In/Register</a>';
+        }
+      ?>
+      <a href="Skills.php">Skills and Achievements</a>
+      <a href="https://www.linkedin.com/in/parth-shah-2025/">LinkedIn</a>
+    </div>
+  </aside>
+  <article>
+    <section>
+      <header>
+        <h1>About Myself</h1>
+      </header>
+      <p>My name is Parth Shah and I am a first year student within the School of Engineering and Computer Science(eecs) at Queen Mary University Of London.<br>
+      <br>I was born and brought up in Edgware (in the borough of Harrow - part North West London). My favourite subjects to study about are: Computer Science,<br>
+      <br>Biology, Chemistry, Maths and Astronomy. My hobbies include playing video games creating mods for video games and anything tech related. I also<br>
+      <br> consider myself a devout hindu and I love debating philosophy with friends.</p>
+    </section>
+    <figure>
+      <img src="parth.jpg">
+      <figcaption>Parth Shah</figcaption>
+    </figure>
+    <section>
+      <header>
+        <h1>Education<h1>
+      </header>
+      <ul>
+        <li>2004-2012- Stag Lane Junior School</li><br>
+        <li>2012-2019- Canons High School</li><br>
+        <li>2019-2022- Queen Mary University Of London</li>
+      </ul>
+    </section>
+    <section>
+      <header>
+        <h1>Qualifications<h1>
+      </header>
+        <ul>
+          <li>GCSE Computer Science</li>
+          <li>GCSE Mathematics</li>
+          <li>GCSE English Language</li>
+          <li>GCSE English Literature</li>
+          <li>GCSE Core, Additional and Futher Additional Science</li>
+          <li>GCSE Geography</li>
+          <li>GCSE RS</li>
+          <li>GCSE Engineering</li>
+          <li>GCSE ICT</li><br>
+          <li>A LEVEL Computer Science</li>
+          <li>A LEVEL Biology</li>
+          <li>A LEVEL Chemistry</li><br>
+          <li>BSC Computer Science(current)</li>
+        <ul>
+    </section>
+  </article>
+  <hr>
+  <footer>
+    <p>© 2020 Parth Shah | <a class="footercl" href="https://www.qmul.ac.uk/">QMUL</a></p>
+  </footer>
+</body>
+</html>

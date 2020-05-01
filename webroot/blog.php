d4e8ffa1d226585ab906c607f4e97c9e2b82f5d5
ParthShah101 <parthshah2065@outlook.com>
2020-05-01 18:11:25 +0100

    changes

--- webroot/blog.php
+++ webroot/blog.php
@@ -35,7 +35,7 @@
             $date = $currentrow['post_date'];
             if($_SESSION['super'] == true)
             {
-              echo "<br><h3>Post ID: $id <br><br> Title: $title<h3><br><p>$text</p><br><p>Time: $date</p><br><a href='delete.html'></a></form><br><hr>";
+              echo "<br><h3>Post ID: $id <br><br> Title: $title<h3><br><p>$text</p><br><p>Time: $date</p><br><a href='delete.html'></a><br><hr>";
             }
             else
             {
@@ -58,7 +58,7 @@
           {
             if($_SESSION['super'] == true)
             {
-              echo'<br><a href="addpost.html">Add Post<a>';
+              echo'<br><a href="addpost.html">Add Post<a><br><a href="delete.html"></a>';
             }
           }
         }

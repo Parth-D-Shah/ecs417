33cef9f05badbe527ec5729300f4f7d6fbe3abcd
ParthShah101 <parthshah2065@outlook.com>
2020-05-01 18:29:15 +0100

    changes

--- webroot/logout.php
+++ webroot/logout.php
@@ -2,6 +2,5 @@
   session_start();
   $_SESSION['logged_in'] = false;
   $_SESSION['super'] = false;
-  header("Location: Homepage.php");
   echo "<script>window.alert('You have succesfully logged out');document.location='Homepage.php'</script>";
 ?>

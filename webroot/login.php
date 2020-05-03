ac80793348b40b59fbc45701bc0669d7fa51a31f
ParthShah101 <parthshah2065@outlook.com>
2020-05-03 01:48:38 +0100

    changed

--- webroot/login.php
+++ webroot/login.php
@@ -21,7 +21,7 @@
   }
   if(isset($_SESSION['logged_in']) && ($_SESSION['logged_in'] == true))
   {
-    if(($username == admin@parth.com)&&($password == root))
+    if(($username == 'admin@parth.com')&&($password == root))
     {
       $_SESSION['super'] = true;
     }

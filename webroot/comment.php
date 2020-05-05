a3b2c64f464f42cf10010c80334b01620a45bc50
ParthShah101 <parthshah2065@outlook.com>
2020-05-05 21:40:53 +0100

    changed

--- webroot/comment.php
+++ webroot/comment.php
@@ -25,7 +25,7 @@
       $date = $row['post_date'];
       if($_SESSION['super'] == true)
       {
-        echo "<br><h3>Post ID: $id <br><br> Title: $title<h3><br><p>$text</p><br><p>Time: $date</p><br><hr>";
+        echo "<br><h3>Post ID: $id <br><br> Title: $title<h3><br><p>$text</p><br><p>Time: $date</p><br><form method='POST' action='commentprocess.php'><input type='hidden' name='id' value='$id'><label>Enter Comment</label><br><textarea name='text' rows='10' cols='50' required='true'></textarea><br><input type='submit'></form><br><hr>";
       }
       else
       {

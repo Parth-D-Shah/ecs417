0c094d1ba00b1f200b00ee28746ef35b29f6bc4f
ParthShah101 <parthshah2065@outlook.com>
2020-05-05 12:38:09 +0100

    changed

---
+++ webroot/comment.php
@@ -0,0 +1,10 @@
+<?php
+  session_start();
+    $dbhost = getenv("MYSQL_SERVICE_HOST");
+    $dbport = getenv("MYSQL_SERVICE_PORT");
+    $dbuser = getenv("DATABASE_USER");
+    $dbpwd = getenv("DATABASE_PASSWORD");
+    $dbname = getenv("DATABASE_NAME");
+    $db = new mysqli($dbhost, $dbuser, $dbpwd, $dbname) or die("Cant Connect to database");
+    echo'test working!';
+    ?>

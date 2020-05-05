c09ccc69492ed9d9311843a11c7b15d562e96374
ParthShah101 <parthshah2065@outlook.com>
2020-05-06 00:00:57 +0100

    changed

---
+++ webroot/commentdelete.php
@@ -0,0 +1,19 @@
+<?php
+  session_start();
+  $dbhost = getenv("MYSQL_SERVICE_HOST");
+  $dbport = getenv("MYSQL_SERVICE_PORT");
+  $dbuser = getenv("DATABASE_USER");
+  $dbpwd = getenv("DATABASE_PASSWORD");
+  $dbname = getenv("DATABASE_NAME");
+  $db = new mysqli($dbhost, $dbuser, $dbpwd, $dbname) or die("Cant Connect to database");
+  $data = $_POST['name'];
+  $sql = "DELETE FROM comment WHERE id=$data";
+  if ($db->query($sql) === TRUE)
+  {
+    header('Location: blog.php');
+  }
+  else
+  {
+    echo "Error: " . $sql . "<br>" . $db->error;
+  }
+?>
